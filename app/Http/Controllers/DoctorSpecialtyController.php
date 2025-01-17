ef9005e8268f66c1bc0cb02b6408506fcafc7411
Hưng Việt Nguyễn <110956729+hunghero32@users.noreply.github.com>
2025-01-17 10:02:37 +0700

    Revert "Tạo Base Trang quản lí Admin"

---
+++ app/Http/Controllers/DoctorSpecialtyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DoctorSpecialty;
+use App\Http\Requests\StoreDoctorSpecialtyRequest;
+use App\Http\Requests\UpdateDoctorSpecialtyRequest;
+
+class DoctorSpecialtyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDoctorSpecialtyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DoctorSpecialty $doctorSpecialty)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DoctorSpecialty $doctorSpecialty)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDoctorSpecialtyRequest $request, DoctorSpecialty $doctorSpecialty)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DoctorSpecialty $doctorSpecialty)
+    {
+        //
+    }
+}
