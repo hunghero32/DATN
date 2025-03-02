6e79290f71e39c13347149aaf99f86dd0c8a442f
Bacnx01 <Nguyenxuanbac110@gmail.com>
2025-03-02 20:51:56 +0700

    ui/ux Doctor

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,25 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/slices/userSlice";
 import doctorReducer from "../features/slices/doctorSlice";
-import guestReducer from "../features/slices/guestSlice";
-import serviceReducer from "../features/slices/serviceSlice";
-import bookingReducer from "../features/slices/bookingSlice";
-import resultReducer from "../features/slices/resultSlice";
-import medicalRecordReducer from "../features/slices/medicalRecordSlice";
-import scheduleReducer from "../features/slices/scheduleSlice";
-import invoiceReducer from "../features/slices/invoiceSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     doctor: doctorReducer,
-    guest: guestReducer,
-    service: serviceReducer,
-    booking: bookingReducer,
-    result: resultReducer,
-    medicalRecord: medicalRecordReducer,
-    schedule: scheduleReducer,
-    invoice: invoiceReducer,
   },
 });
 
