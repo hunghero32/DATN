fc3a6cadd9d8a4e70fa2194d341ba23f9a8b7086
Đỗ Đức Anh <149552709+anhnopro@users.noreply.github.com>
2025-03-10 09:26:38 +0700

    api detail service

--- backend/app/Models/Doctor.php
+++ backend/app/Models/Doctor.php
@@ -32,4 +32,8 @@ class Doctor extends Model
     {
         return $this->belongsTo(Specialty::class);
     }
+    public function schedules()
+    {
+        return $this->hasMany(Schedule::class, 'doctor_id', 'id');
+    }
 }
--- backend/routes/client_api.php
+++ backend/routes/client_api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\Client\DetailServiceController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Client\HomeController;
 
 Route::get('/home', [HomeController::class, 'index']);
+Route::get('/detail-service-{id}', [DetailServiceController::class, 'detailService']);
