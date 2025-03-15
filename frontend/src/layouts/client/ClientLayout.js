c04968434abf5ee8e43c2ae9f65cac394d8e1da8
dongnv311 <dongnvph31167@fpt.edu.vn>
2025-03-15 19:02:30 +0700

    frontend/ApiHienThiChiTietService

--- frontend/src/layouts/client/ClientLayout.js
+++ frontend/src/layouts/client/ClientLayout.js
@@ -39,6 +39,7 @@ import Services from "../../components/guest/home/Service";
 import ChiTietDatLich from "../../components/guest/home/ChiTietDatLich";
 import DatLich from "../../components/guest/home/DatLich";
 import ChiTietChuyenKhoa from "../../components/guest/home/ChiTietChuyenKhoa";
+import ServiceDetail from "../../components/guest/home/ServiceDetail";
 
 const ClientLayout = () => {
   useEffect(() => {
@@ -105,8 +106,9 @@ const ClientLayout = () => {
         <Route path="/specialties" element={<TopSpecialties/>}/> 
         <Route path="/topServices" element={<TopBookedServices/>}/>
         <Route path="/clinicDetail" element={<ClinicDetail/>}/>
-        <Route path="/datlich" element={<DatLich/>}/>  {/* Trang đặt lịch khám */}
+        <Route path="/booking/:serviceId" element={<DatLich />} /> {/* Trang đặt lịch khám */}
         <Route path="/services" element={<Services/>}/>  {/* Trang dịch vụ */}
+        <Route path="/detail-service/:id" element={<ServiceDetail/>}/>  {/* Trang chi tiet dich vu  */}
         <Route path="/contact" element={<HomeMainContact />} />
         {/* Update the route path to match the navigation */}
         <Route path="/detail-specialty/:id" element={<ChiTietChuyenKhoa />} />
