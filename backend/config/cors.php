8a6bdf789f629c4024f8dde3625ccc162745da46
Hưng Việt Nguyễn <110956729+hunghero32@users.noreply.github.com>
2025-03-28 23:33:42 +0700

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
