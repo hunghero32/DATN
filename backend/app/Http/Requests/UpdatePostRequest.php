9fb9c2a1c9a89eb4afb269f24a7df3792d8d08d9
Hưng Việt Nguyễn <110956729+hunghero32@users.noreply.github.com>
2025-04-02 12:55:30 +0700

    Update UpdatePostRequest.php

--- backend/app/Http/Requests/UpdatePostRequest.php
+++ backend/app/Http/Requests/UpdatePostRequest.php
@@ -17,8 +17,6 @@ class UpdatePostRequest extends FormRequest
             'category_id' => 'sometimes|required|integer|exists:categories,id',
             'user_id' => 'sometimes|required|integer|exists:users,id',
             'slug' => 'sometimes|required|string|max:255|unique:posts,slug,' . $this->route('post')->id,
-            dd($this->route('post')),
-
             'title' => 'sometimes|required|string|max:255',
             'content' => 'sometimes|required|string',
             'views' => 'nullable|integer|min:0',
