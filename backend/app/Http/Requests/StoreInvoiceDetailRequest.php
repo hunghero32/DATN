f2fcacb96fb2b526efa2a01e4c6a3d5da8ba4537
Đỗ Đức Anh <149552709+anhnopro@users.noreply.github.com>
2025-05-28 15:05:03 +0700

    fix lỗi hóa đơn

--- backend/app/Http/Requests/StoreInvoiceDetailRequest.php
+++ backend/app/Http/Requests/StoreInvoiceDetailRequest.php
@@ -14,7 +14,7 @@ class StoreInvoiceDetailRequest extends FormRequest
     public function rules()
     {
         return [
-            'invoice_id' => 'required|exists:invoices,id',
+            // 'invoice_id' => 'required|exists:invoices,id',
             'booking_id' => 'required|exists:bookings,id',
             'discount'   => 'nullable|numeric|min:0',
             'tax'        => 'nullable|numeric|min:0|max:100',
@@ -24,14 +24,14 @@ class StoreInvoiceDetailRequest extends FormRequest
     public function messages()
     {
         return [
-            'invoice_id.required' => 'Vui lòng chọn hóa đơn.',
-            'invoice_id.exists' => 'Hóa đơn không tồn tại.',
+            // 'invoice_id.required' => 'Vui lòng chọn hóa đơn.',
+            // 'invoice_id.exists' => 'Hóa đơn không tồn tại.',
             'booking_id.required' => 'Vui lòng chọn đặt lịch.',
             'booking_id.exists' => 'Đặt chỗ không tồn tại.',
             'discount.numeric' => 'Giảm giá phải là số.',
             'tax.numeric' => 'Thuế phải là số.',
             'tax.min' => 'Thuế không được nhỏ hơn 0%.',
-            'tax.max' => 'Thuế không được lớn hơn 100%.',        
+            'tax.max' => 'Thuế không được lớn hơn 100%.',
         ];
     }
 }
